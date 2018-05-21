c5b24bc4239303746f6cd41ef448e65f456989b6
hirojiang <Jan@Hiromasa-no-MacBook-Air-2.local>
2018-05-21 16:01:53 +0900

    before start adding chat space function

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+describe MessagesController do
+  # letを利用してテスト中使用するインスタンスを定義
+  let(:group) { create(:group)}
+  let(:user) { create(:user)}
+
+  describe '#index' do
+    # ログインしている場合
+    context 'log in' do
+      # 「ログインをする」、「擬似的にindexアクションを動かすリクエストを行う」という共通の処理をbeforeブロックにまとめている。各examl eが実行される直前に、毎回実行される。
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+      # アクション内で定義しているインスタンス変数があるか
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+      # 該当するビューが描画されているか
+      it 'render index' do
+        expect(response).to render_template :index
+      end
+
+    end
+    # ログインしていない場合
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+      # 意図したビューにリダイレクトできているか
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    let(:params) { {group_id: group.id, user_id: user.id, message: attributes_for(:message)}}
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params){{group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil)}}
+        subject{
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{subject}.not_to change(Message, :count)
+        end
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
