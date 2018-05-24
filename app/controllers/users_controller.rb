deff4b97552cc30346b5e1761415c0a8305f006f
hirojiang <Jan@Hiromasa-no-MacBook-Air-2.local>
2018-05-24 16:27:10 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").where.not(id:current_user.id)
+    respond_to do |format|
+      format.html {redirect_to root_path}
+      format.json
+    end
   end
 
   def edit
@@ -15,9 +20,7 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
