eb7b21bd4428ea9ada93c15c4818092c61aeecc3
hirojiang <Jan@Hiromasa-no-MacBook-Air-2.local>
2018-05-24 16:52:27 +0900

    #2 インクリメンタルサーチ

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -26,9 +26,7 @@ $(function(){
               <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                 <input name='group[user_ids][]' type='hidden' value='${userId}'>
                 <p class='chat-group-user__name'>${userName}</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>
-                  削除
-                </a>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
               </div>
                `
     groupMemberList.append(html)
